ce999ced83b332ef3a804ad3e33b26c06bfee579
Sudeepachanda <susanka@translense.com>
2024-12-04 15:59:21 +0530

    BusinessInvestmentApp

---
+++ Business-Investment-App/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header id="header">
+      <img
+        src="src\components\Header\investment-calculator-logo.png"
+        alt="Logo Showing a Money Bag"
+      />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+};
+
+export default Header;
