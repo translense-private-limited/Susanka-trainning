c3b9c78dcab1a721603478f249313b877711ea55
Sudeepachanda <susanka@translense.com>
2024-12-04 16:01:41 +0530

    Styling-CSS

---
+++ StylingCss/src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./components/Header";
+import AuthInputs from "./components/AuthInputs";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <AuthInputs />
+    </>
+  );
+}
+
+export default App;
